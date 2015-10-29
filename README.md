e842442cf68e0f1f3e44a68f74182dc6eee1a3d8
Diheng Zhang <dihengz@gmail.com>
2015-10-29 15:27:45 -0400

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PIExporter
 A python application that drag data to server and secue them automatically
+
+What this little application is going to do:
+
+1\ Read in a data package (let's say, d).
+
+2\ One by one, read out the data form names(scale names) in d, and then:
+
+A\ Check if there is a file named [form_name]_[date].csv in the Active Data Pool, if not, create one
+
+B\ Open [form_name]_[date].csv, append the data we have into it, one by one. [And then send back delete commend one by one?]
+
+C\ Close file, Log down the action and output to log.txt. If there is an error, email me.
+ 
---
+++ export.py
@@ -0,0 +1,61 @@
+# Install the package is needed:
+
+
+# Import the Requests
+
+import requests
+import time
+import os.path
+
+# Set up the server link:
+# Use this link for testing prahse 
+SERVER = 'http://localhost:9000/public/export'
+
+# Use this link for actual prahse
+# SERVER = 'http://localhost:9000/public/export'
+
+# Get the date
+DATE = time.strftime("_%d_%m_%Y")
+
+# Read the data
+r = requests.get(SERVER)
+
+data = r.json()
+
+# One by one, read out the data form names(scale names) in d, and then:
+
+for scale in data:
+    quest = requests.get(SERVER+'/'+scale['name'])
+    quest = quest.json()
+    if scale['size'] != 0:
+        ks = list(quest[0].keys())
+        ks.sort()
+
+#A\ Check if there is a file named [form_name]_[date].csv in the Active Data Pool, if not, create one 
+
+        if not os.path.exists('active_data/'+scale['name']+DATE+'.csv'):
+            output = open('active_data/'+scale['name']+DATE+'.csv','w')
+            for heads in ks[:-1]: 
+                output.write(heads+'/')
+            output.write(ks[-1]+'\n')
+            output.close()
+
+#B\ Open [form_name]_[date].csv, append the data we have into it, one by one. 
+        
+        output = open('active_data/'+scale['name']+DATE+'.csv','a')
+        for item in quest:
+            for key in ks[:-1]:
+                output.write(str(item[key])+'/')
+            output.write(str(item[ks[-1]])+'\n')
+            if scale['deleteable']: 
+                requests.delete(SERVER+'/'+scale['name']+'/'+str(item['id']))
+        output.close()
+
+#[And then send back delete commend one by one]
+
+            
+
+
+
+
+
