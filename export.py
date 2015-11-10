27f198eda8eac61a50329db2c39c1938e5082032
Dan Funk <dan.funk@twintechs.com>
2015-11-10 15:27:58 -0500

    We will need to unencrypt the participant id after export.

--- export.py
+++ export.py
@@ -5,16 +5,18 @@
 
 # Import the Requests
 
-import requests
+import requests # To make REST requests of the client.
 import time
 import os.path
 import sys
+import rsa # To decrypt values.
 
 # Set up the server link:
 # Use this link for testing phrase 
 SERVER='http://localhost:9000/api/export'
 USER='daniel.h.funk@gmail.com'
 PASS='ereiamjh'
+PRIVATE_FILE='private_key.pem'
 
 # Use this link for actual phrasse
 # SERVER = 'http://localhost:9000/admin/export'
@@ -22,10 +24,22 @@ PASS='ereiamjh'
 # Get the date
 DATE = time.strftime("_%d_%m_%Y")
 
+
+
 def notify_admin(e):
     print("YO ADMIN! This should be an email, the problem is:" + str(e))
 
 
+
+with open(PRIVATE_FILE) as privatefile:
+    keydata = privatefile.read()
+priv_key = rsa.PrivateKey.load_pkcs1(keydata)
+def decrypt(crypto):
+    if crypto is None: return ""
+    message = rsa.decrypt(crypto.decode('base64'), priv_key)
+    return message.decode('utf8')
+    
+    
 # DF: Should likely write our own method that will make the request and return
 # a json response, since things can go wrong here in lots of ways and we want
 # to try and catch all of them. In this way we can handle exceptions, emailing
@@ -63,14 +77,17 @@ for scale in data:
                 output.write(heads+'\t')
             output.write(ks[-1]+'\n')
             output.close()
-
+            
 #B\ Open [form_name]_[date].csv, append the data we have into it, one by one. 
         
         output = open('active_data/'+scale['name']+DATE+'.csv','a')
         for item in quest:
-            for key in ks[:-1]:
-                output.write((item[key].encode('utf-8') if isinstance(item[key],unicode) else str(item[key]))+'\t')
-            output.write((item[ks[-1]].encode('utf-8') if isinstance(item[ks[-1]],unicode) else str(item[ks[-1]]))+'\n')
+            for key in ks:                
+                if(key.endswith("RSA")): value = decrypt(item[key])
+                elif item[key] is None: value = ""
+                else: value = str(item[key]) # could be an int, make sure it is a string so we can encode it.
+                output.write(value.encode('utf-8') + "\t")
+            output.write("\n")
             #[And then send back delete commend one by one]            
 #            if scale['deleteable']: 
 #                requests.delete(SERVER+'/'+scale['name']+'/'+str(item['id']))
