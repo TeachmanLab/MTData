9f51a221c59c0137b346d335195cf7fc4672ba21
Diheng Zhang <dihengz@gmail.com>
2016-10-04 14:47:01 -0400

    New Start

--- MTData/export_old.py
+++ MTData/export_old.py
@@ -20,14 +20,10 @@ import json
 
 
 # Set up logging config files
-# Setting up files directories and logging config files
-if not os.path.exists("/Users/diheng/Box Sync/logs/"): # Can't find a better way to do this......
-    os.makedirs("/Users/diheng/Box Sync/logs/")
 logging.config.dictConfig(yaml.load(open('log.config', 'r')))
 
 # Load the Configuration file
 SERVER_CONFIG = 'server.config'
-newDay = False
 
 
 # Decrypting
@@ -42,10 +38,16 @@ def decrypt(crypto, id, scaleName, field, priv_key):
             log.info('Decrypt successfully.')
             return message.decode('utf8')
         except (rsa.pkcs1.CryptoError, rsa.pkcs1.DecryptionError):
-            log.error('Decrypt failed, original value recorded. Server = %s, Questionnaire = %s, Entry ID: %s, Field: %s See information:', config['SERVER'], scaleName, id, field, exc_info = 1)
+            log.error('Decrypt failed, original value recorded. Questionnaire = %s, Entry ID: %s, Field: %s See information:', scaleName, id, field, exc_info = 1)
             return crypto
     except (UnicodeDecodeError, binascii.Error):
-        log.error('Decode failed, item skipped. Server = %s, Questionnaire = %s, Entry ID: %s, Field: %s See information:', config['SERVER'], scaleName, id, field, exc_info = 1)
+        log.error('Decode failed, item skipped. Questionnaire = %s, Entry ID: %s, Field: %s See information:', scaleName, id, field, exc_info = 1)
+
+
+# Setting up files directories and logging config files
+if not os.path.exists(config["PATH"]+"logs/"): # Can't find a better way to do this......
+    os.makedirs(config["PATH"]+"logs/")
+logging.config.dictConfig(yaml.load(open('log.config', 'r')))
 
 
 # ------------------------------------------#
@@ -59,7 +61,7 @@ def safeKeep(scaleName, response, file, config):
            log.info(scaleName + " data backup successfully.")
         return True
     except:
-        log.critical('Server: %s.' + scaleName + ' data backup failed, immediate attention needed.\n', config['SERVER'], exc_info = 1)
+        log.critical(scaleName + ' data backup failed, immediate attention needed.\n', exc_info = 1)
         return False
 
 # DF: Should likely write our own method that will make the request and return
@@ -80,13 +82,13 @@ def safeRequest(url, config):
                 log.info("We got something new! Let's have a closer look!")
                 return response
             except:
-                log.critical("Server: %s. Can't read data in json form. Did not receive a json response, perhaps log-in credentials are incorrect? See below for error information:\n", config['SERVER'], exc_info = 1)
+                log.critical("Can't read data in json form. Did not receive a json response, perhaps log-in credentials are incorrect? See below for error information:\n", exc_info = 1)
     except requests.exceptions.Timeout:
         log.critical("Data request timed out for url: " + url + ". See below for error information:\n", exc_info = 1)
     except requests.exceptions.TooManyRedirects:
         log.critical("Too many redirects for url: " + url + ". See below for error information:\n", exc_info = 1)
     except requests.exceptions.RequestException as e:  # DF: We may loose some detail here, better to check all exceptions.
-        log.critical("Server: %s. Data request failed, fatal, emailed admin. Error was " + str(e) + " see below for error information:\n", config['SERVER'], exc_info = 1)
+        log.critical("Data request failed, fatal, emailed admin. Error was " + str(e) + " see below for error information:\n", exc_info = 1)
 
 
 # SafeDelete function, use this to delete data entries and log down system message
@@ -100,7 +102,7 @@ def safeDelete(url, config):
         log.info("Data delete successfully, see below for request detail:\n%s\nIssues: %s", url, str(m)) # Log successful data delete
         return True
     except requests.exceptions.RequestException as e:  # DF: We may loose some detail here, better to check all exceptions.
-        log.critical("Server: %s. Data delete failed, fatal, emailed admin. see below for error information:\n", config['SERVER'], exc_info = 1)
+        log.critical("Data delete failed, fatal, emailed admin. see below for error information:\n", exc_info = 1)
         return False
 
 
@@ -126,7 +128,7 @@ def safeWrite(response, ks, scaleName, deleteable, config):
             benchMark = json.load(benchMarkJson)
         log.info("benchMark information successfully retrived.")
     except:
-        log.critical("Server: %s. benchMark information retrived failed, immediate attention needed. Detail:\n", config['SERVER'], exc_info = 1)
+        log.critical("benchMark information retrived failed, immediate attention needed. Detail:\n", exc_info = 1)
 # Writing starts here:
     with open(date_file, 'a') as datacsv:
         dataWriter = csv.DictWriter(datacsv, dialect='excel', fieldnames= ks)
@@ -150,13 +152,13 @@ def safeWrite(response, ks, scaleName, deleteable, config):
                         try:
                             value = str(entry[key]) # could be an int, make sure it is a string so we can encode it.
                         except:
-                            log.error("Data encode failed, data lost. Server: %s, Questionnaire: %s, Entry ID: %s, Field: %s",config['SERVER'], scaleName, entry['id'], key, exc_info = 1) # Should log error, entry ID and data field
+                            log.error("Data encode failed, data lost. Questionnaire: %s, Entry ID: %s, Field: %s", scaleName, entry['id'], key, exc_info = 1) # Should log error, entry ID and data field
                     if (value != None):
                         try:
                             entry[key] = value.encode('utf-8')
                             log.debug("Data successfully encoded.")
                         except UnicodeEncodeError:
-                            log.error("Data encode failed, data lost. Server: %s, Questionnaire: %s, Entry ID: %s, Field: %s",config['SERVER'], scaleName, entry['id'], key, exc_info = 1) # Should log error, entry ID and data field
+                            log.error("Data encode failed, data lost. Questionnaire: %s, Entry ID: %s, Field: %s", scaleName, entry['id'], key, exc_info = 1) # Should log error, entry ID and data field
                     else: entry[key] = ""
                 try:
                     dataWriter.writerow(entry)
@@ -167,21 +169,21 @@ def safeWrite(response, ks, scaleName, deleteable, config):
                     else: log.info('Questionnaire - %s, ID - %s, data cleaning on hold. Detail: Deleteable: %s, Backup: %s, Delete Mode: %s', scaleName, str(entry['id']), str(deleteable), str(backup), str(config["DELETE_MODE"]))
                 except csv.Error:
                     error += 1
-                    log.critical("Failed in writing entry, Server: %s. Questionnaire: %s, Entry ID: %s", config['SERVER'], scaleName, str(entry['id']), exc_info = 1)
+                    log.critical("Failed in writing entry, Questionnaire: %s, Entry ID: %s", scaleName, str(entry['id']), exc_info = 1)
             else:
                 log.info('Questionnaire - %s, ID - %s, data writing skipped. Reason: Not a new entry.', scaleName, str(entry['id']))
-                if deleteable : log.critical('Warning: Previous sensitive data is found on server %s, please delete it ASAP. Detail: Questionnaire - %s, ID - %s. ', config['SERVER'], scaleName, str(entry['id'])) #
+                if deleteable : log.critical('Warning: Previous sensitive data is found on server, please delete it ASAP. Detail: Questionnaire - %s, ID - %s. ', scaleName, str(entry['id'])) #
         log.info("Questionnaire %s update finished - %s new entries recoded successfully.", scaleName, str(t))
         log.info("Questionnaire %s data cleaning finished - %s new entries successfully deleted on MindTrails.", scaleName, str(d))
         if newBenchMark > benchMark[scaleName]: benchMark[scaleName] = newBenchMark # Update the lastest ID after all the new data writing
         if error > 0:
-            log.critical("Server: %s, Questionnaire %s update error - %s new entries failed to recode.", config['SERVER'], scaleName, str(error))
+            log.critical("Questionnaire %s update error - %s new entries failed to recode.", scaleName, str(error))
     try:
         with open(config["PATH"]+'active_data/benchMark.json',"wb") as benchMarkJson:
             json.dump(benchMark,benchMarkJson)
         log.info("benchMark information update successfully.")
     except:
-        log.warning("Server: %s. BenchMark information update failed, duplicated record may created, please check:\n", config['SERVER'], exc_info = 1)
+        log.warning("benchMark information update failed, duplicated record may created, please check:\n", exc_info = 1)
 
 
 # Create data files with date as name:
@@ -218,7 +220,7 @@ def safeExport(data,config):
                 else: log.info("No new entries found in %s", str(scale['name']))
             else:
                 log.warning("""This is weired... It seems that there is nothing out there or I am blocked from MindTrails. You might already get an email from me
-                reporting some network issues. Be alerted, stay tuned. Server: %s""", config['SERVER'])
+                reporting some network issues. Be alerted, stay tuned.""")
     log.info("Database update finished: %s questionnaires' data updated.", str(s))
 
 
@@ -232,7 +234,7 @@ def pathCheck(config):
             os.makedirs(config["PATH"]+"active_data/")
             log.info("Successfully created active_data folder.")
     except:
-        log.critical("Server: %s. Failed to create data or log files, fatal, emailed admin.", config['SERVER'], exc_info=1)
+        log.critical("Failed to create data or log files, fatal, emailed admin.", exc_info=1)
 
 # ------------------------------------------#
 # This is the main module
@@ -250,22 +252,15 @@ def export(config):
         if (oneShot.json() != None): safeExport(oneShot.json(),config)
     else:
         log.warning("""This is weired... It seems that there is nothing out there or I am blocked from MindTrails. You might already get an email from me
-     reporting some network issues. Be alerted, stay tuned. Server: %s.""",config['SERVER'])
+     reporting some network issues. Be alerted, stay tuned.""")
     log.info("I am tired and I am going back to Laura's server for a rest. See you later!")
 
 # This is a over all program
 def martin(task_list):
-    log = logging.getLogger('martin')
-    try:
-        address = yaml.load(open(task_list, 'r'))
-        log.info('Address book read successfully.')
-        for key in address:
-            config = address[key]
-            log.info('Address for export: %s. Ready?: %s',str(key),str(config['READY']))
-            if config['READY']: export(config)
-    except:
-        log.info('Address book read failed. Emailed admin.', exc_info=1)
-
+    address = yaml.load(open(task_list, 'r'))
+    for key in address:
+        config = address[key]
+        if config['ON']: export(config)
 
 # Works here:
 martin(SERVER_CONFIG)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mindtrails',
+      version='0.1',
+      description='Toolbox for MindTrails Project',
+      url='http://github.com/Diheng/mindtrails',
+      author='Diheng Zhang',
+      author_email='dzhang@virginia.edu`',
+      license='MIT',
+      packages=['mindtrails'],
+      zip_safe=False)
