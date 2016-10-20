68a34b62f54c7c427ac0c1463a0205d46afe4e5d
Diheng Zhang <dihengz@gmail.com>
2016-10-20 10:26:17 -0400

    trying out

--- setup.py
+++ setup.py
@@ -1,14 +1,47 @@
+#!/usr/bin/env python
+
+PROJECT = 'MTData'
+
+# Change docs/sphinx/conf.py too!
+VERSION = '0.1'
+
+
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
 
-setup(name='MTData',
-      version='0.1',
+try:
+    long_description = open('README.rst', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(name=PROJECT,
+      version=VERSION,
       description='Data Toolbox for MindTrails Projects',
+      long_description=long_description,
+      install_requires=['cliff'],
       url='http://github.com/Diheng/MTData',
       author='Diheng Zhang, Dan Funk, Sam Portnow',
       author_email='dzhang@virginia.edu`',
       license='MIT',
-      packages=['MTData'],
-      zip_safe=False)
+      namespace_packages=[],
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+        'console_scripts': [
+            'MTData = MTData.main:main'
+        ],
+        'MTData': [
+            'export = MTData.export:export',
+            'recover = MTData.recover:recovery',
+            'error = cliffdemo.simple:Error',
+            'list files = cliffdemo.list:Files',
+            'files = cliffdemo.list:Files',
+            'file = cliffdemo.show:File',
+            'show file = cliffdemo.show:File',
+            'unicode = cliffdemo.encoding:Encoding',
+        ],
+    },
+      )
